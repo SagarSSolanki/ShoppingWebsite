9b51c839817dcbf6364fbf92b170bf62b158017b
SagarSSolanki <ssolanki@grepruby.io>
2022-02-15 11:29:35 +0530

    Spaces fixed

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,27 +1,27 @@
 class RegistrationsController < ApplicationController
-    def index
-        if session[:user_id]
-            @user = User.find_by(id: session[:user_id])
-        end
+  def index
+    if session[:user_id]
+      @user = User.find_by(id: session[:user_id])
     end
+  end
  
-    def new
-        @user = User.new 
+  def new
+    @user = User.new 
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      session[:user_id] = @user.id  
+      redirect_to root_path, notice: "Successful"
+    else
+      render :new
     end
+  end
 
-    def create
-        @user = User.new(user_params)
-
-        if @user.save
-            session[:user_id] = @user.id  
-            redirect_to root_path, notice: "Successful"
-        else
-            render :new
-        end
-    end
-
-    private
-    def user_params
-        params.require(:user).permit(:email, :password, :password_confirmation)
-    end    
+  private
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
+  end    
 end
