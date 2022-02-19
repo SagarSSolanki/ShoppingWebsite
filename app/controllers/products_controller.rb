49dd2b596499df706d15b4ecb554d5519b42dd3b
SagarSSolanki <ssolanki@grepruby.io>
2022-02-19 23:58:12 +0530

    commit products 1.0

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,26 @@
+class ProductsController < ApplicationController
+  def index
+    @products = Product.all
+    @user = current_user
+  end
+
+  def new 
+    @product = Product.new
+  end
+
+  def create 
+    @product = Product.new(product_params)
+
+    if @product.save
+      redirect_to products_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :price, :stock, :category_id)
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_10_213706) do
-  
+ActiveRecord::Schema.define(version: 2022_02_17_202121) do
+
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -26,6 +26,22 @@ ActiveRecord::Schema.define(version: 2022_02_10_213706) do
     t.index ["user_id"], name: "index_addresses_on_user_id"
   end
 
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.integer "price"
+    t.integer "stock"
+    t.bigint "category_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["category_id"], name: "index_products_on_category_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.string "password_digest"
@@ -34,4 +50,5 @@ ActiveRecord::Schema.define(version: 2022_02_10_213706) do
   end
 
   add_foreign_key "addresses", "users"
+  add_foreign_key "products", "categories"
 end
