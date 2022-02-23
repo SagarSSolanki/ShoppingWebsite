2d2e86830bd716ad676d4baa2097b1029a35eff9
SagarSSolanki <ssolanki@grepruby.io>
2022-02-23 15:37:36 +0530

    First Image Displayed

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -48,7 +48,7 @@ class ProductsController < ApplicationController
   private
 
   def product_params
-    params.require(:product).permit(:name, :price, :stock, :category_id)
+    params.require(:product).permit(:name, :price, :stock, :category_id, :image)
   end
 
   def is_admin
