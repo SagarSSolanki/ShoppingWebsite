f94c0b25b104d0af7684fa479bf562e206d0b24f
SagarSSolanki <98022912+SagarSSolanki@users.noreply.github.com>
2022-03-14 10:51:07 +0530

    Background job (#9)
    * Test
    
    * Branch Ready

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -70,6 +70,7 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  config.active_job.queue_adapter = :sidekiq
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   config.action_mailer.raise_delivery_errors = true
